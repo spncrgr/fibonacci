b495b6043ed214cf30fbede2603c7cd98a8245a1
Spencer Roach <sroach@theadhdprogrammer.com>
2018-08-25 12:15:43 -0500

    skeleton implementation

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -1,7 +1,23 @@
 require 'fibonacci/version'
 
 class Fibonacci
+
+  SEQUENCE = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144]
+
+  # @example Given a standard Fibonacci sequence, return the value of the sixth entry of the sequence.
+  #   Fibonacci.value_at(6) #=> 13
+  #
+  # @example Negative index values are not supported
+  #   Fibonacci.value_at(-1) #=> RangeError
+  #
+  # @param [Integer] index A zero-based index which applies to a standard Fibonacci sequence. The value at that
+  # location in the sequence is what is returned.
+  # @return [Integer] A value from a standard Fibonacci sequence
   def self.value_at(index)
-    
+    if index < 0
+      raise RangeError,
+            "The elements of a Fibonacci sequence can only be accessed using a positive index (greater than or equal to zero)."
+    end
+    SEQUENCE[index]
   end
 end
--- test/fibonacci_test.rb
+++ test/fibonacci_test.rb
@@ -5,4 +5,10 @@ class FibonacciTest < Minitest::Test
     actual = Fibonacci.value_at(6)
     assert_equal(13, actual)
   end
+
+  def test_that_it_raises_error
+    assert_raises(RangeError) do
+      Fibonacci.value_at(-1)
+    end
+  end
 end
