5eaad26df3ce2954d4b92b9abc94f7b2867332fd
Spencer Roach <sroach@theadhdprogrammer.com>
2018-08-29 19:39:54 -0500

    add second solution using matrix closed-form solution

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -5,19 +5,40 @@ require 'fibonacci/version'
 class Fibonacci
   attr_reader :sequence
 
+  # Create an instance of the Fibonacci class and generate a sequence. The sequence will be available
+  # via Fibonacci.sequence
+  #
+  # @example Create a sequence of 12 Fibonacci numbers
+  #   my_fibonacci = Fibonacci.new(12)
+  #   sequence = my_fibonacci.sequence #=> [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144]
+  #
+  # @param [Integer] sequence_length The number of Fibonacci numbers to generate
+  # @return [Array<Intger>] A sequence of Fibonacci numbers, starting at 1 and continuing
+  # to a length of 'sequence_length'
   def initialize(sequence_length)
-    length = sequence_length - 1
+    if sequence_length <= 0
+      raise ArgumentError, 'The generated sequence must have a length of at least one'
+    end
+
+    length    = sequence_length
+    # length    = sequence_length - 1
     @sequence = Array.new(length, 1)
-    2.step(length) do |i|
-      sequence[i] = sequence[i - 1] + sequence[i - 2]
+    length.times do |i|
+      # Manually set the value of the first two elements of the generated sequence to the number 1
+      if i <= 1
+        @sequence[i] = 1
+        next
+      end
+
+      @sequence[i] = @sequence[i - 1] + @sequence[i - 2]
     end
   end
 
   # @example Given a standard Fibonacci sequence, return the value of the sixth entry of the sequence.
-  #   Fibonacci.value_at(6) #=> 13
+  #   sequence.value_at(6) #=> 13
   #
   # @example Negative index values are not supported
-  #   Fibonacci.value_at(-1) #=> RangeError
+  #   sequence.value_at(-1) #=> RangeError
   #
   # @param [Integer] index A zero-based index which applies to a standard Fibonacci sequence. The value at that
   # location in the sequence is what is returned.
@@ -28,5 +49,28 @@ class Fibonacci
             'The elements of a Fibonacci sequence can only be accessed using a positive index.'
     end
     sequence[index]
+  end
+
+  # @example Given a standard Fibonacci sequence, return the value of the sixth entry of the sequence.
+  #   Fibonacci.value_from_index(6) #=> 13
+  #
+  # @example Generating negative Fibonacci numbers is not supported
+  #   sequence.value_from_index(-1) #=> RangeError
+  #
+  # @param [Integer] index A zero-based index which applies to a standard Fibonacci sequence. The value at that
+  # location is calculated and returned.
+  # @return [Integer] A value from a standard Fibonacci sequence
+  def self.value_from_index(index)
+    if index.negative?
+      raise ArgumentError,
+            'Generating a negative Fibonacci numbers is not supported'
+    end
+    n = index + 1
+
+    coefficient       = 1 / Math.sqrt(5)
+    first_difference  = ((1 + Math.sqrt(5)) / 2) ** n
+    second_difference = ((1 - Math.sqrt(5)) / 2) ** n
+    result            = (coefficient * first_difference) - (coefficient * second_difference)
+    result.round
   end
 end
---
+++ test/solution_one_test.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class SolutionOneTest < Minitest::Test
+  def setup
+    @example_sequence = Fibonacci.new(12)
+  end
+
+  def test_that_it_creates_a_sequence
+    expected = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144]
+    assert_equal(expected, @example_sequence.sequence)
+  end
+
+  def test_that_it_creates_one_number
+    @example_sequence = Fibonacci.new(1)
+    expected = [1]
+    assert_equal(expected, @example_sequence.sequence)
+  end
+
+  def test_that_it_creates_two_numbers
+    @example_sequence = Fibonacci.new(2)
+    expected = [1, 1]
+    assert_equal(expected, @example_sequence.sequence)
+  end
+
+  def test_that_it_returns_index_6
+    actual = @example_sequence.value_at(6)
+    assert_equal(13, actual)
+  end
+
+  def test_that_it_raises_range_error
+    assert_raises(RangeError) do
+      @example_sequence.value_at(-1)
+    end
+  end
+
+  def test_that_it_raises_argument_error
+    assert_raises(ArgumentError) do
+      Fibonacci.new(0)
+    end
+  end
+end
